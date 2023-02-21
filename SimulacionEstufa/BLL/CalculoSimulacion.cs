816b115e4df81535fc9055a4f13fa8cc7e24acd1
pablouix <pablouix@gmail.com>
2023-02-21 08:46:24 -0400

    [Anderson] Agregando cambios a NET 7

---
+++ SimulacionEstufa/BLL/CalculoSimulacion.cs
@@ -0,0 +1,88 @@
+﻿using SimulacionEstufa.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionEstufa.BLL
+{
+    public class CalculoSimulacion
+    {
+        // Definimos el tiempo de actualización de la temperatura en segundos
+        private int tiempoActualizacion = 1;
+
+        public Estufa estufa;
+
+        public CalculoSimulacion(Estufa estufa)
+        {
+            this.estufa = estufa;
+        }
+
+        public double GetTemperatura()
+        {
+            return estufa.temperaturaInicial += TemperaturaIncrementada();
+        }
+
+        // Calculamos la cantidad de calor necesaria para calentar el agua
+        public double CalorNecesario()
+        {
+            return (estufa.temperaturaDeseada - estufa.temperaturaInicial) * estufa.capacidadRecipiente * estufa.calorEspecifico;
+        }
+
+        // Tiempo necesario para calentar el agua en segundos
+        private double TiempoGas()
+        {
+            return CalorNecesario() / estufa.calorEspecifico;
+        }
+
+        // Cantidad de gas necesario en kg
+        private double gasNecesario()
+        {
+            return estufa.presionTanque * estufa.volumenGasTanque * estufa.densidadGas;
+        }
+
+        // Flujo de gas necesario en kg/s
+        private double VolumenGas()
+        {
+            return (gasNecesario() / TiempoGas()) * estufa.eficienciaTanque;
+        }
+
+        // Velocidad del gas en el tubo en m/s
+        public double VelocidadGas()
+        {
+            return VolumenGas() / VolumenManguera();
+        }
+
+        // Calculamos la potencia efectiva de la estufa
+        private double PotenciaEfectiva()
+        {
+            return VelocidadGas() * estufa.eficienciaEstufa;
+        }
+
+        // Calculamos la cantidad de calor generada por la estufa en un segundo
+        private double CalorGenerado()
+        {
+            return PotenciaEfectiva() * tiempoActualizacion;
+        }
+
+        // Calculamos la temperatura incrementada por segundo
+        private double TemperaturaIncrementada()
+        {
+            return CalorGenerado() / (estufa.capacidadRecipiente * estufa.calorEspecifico);
+        }
+
+        // Calculamos el volumen de la manguera conectada al tanque
+        private double VolumenManguera()
+        {
+            /*Área transversal del tubo en metros cuadrados multiplicada por la longitud
+                resultando en su volumen en metros cubicos*/
+            return estufa.longitudManguera * (Math.PI * (estufa.diametroManguera * estufa.diametroManguera) / 4.0);
+        }
+
+        public void Resultado()
+        {
+
+        }
+    }
+}
--- SimulacionEstufa/MainWindow.xaml.cs
+++ SimulacionEstufa/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SimulacionEstufa.BLL;
+using SimulacionEstufa.Entidad;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SimulacionEstufa
 {
@@ -20,8 +24,16 @@ namespace SimulacionEstufa
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int posisicionOrnilla = 0;
-        public int posisicionAperturaTanque = 0;
+        private int posisicionOrnilla = 0;
+        private int posisicionAperturaTanque = 0;
+
+        Estufa estufa = new Estufa();
+        CalculoSimulacion simulacion;
+
+        float[] eficiencia = { 0.0f, 0.2f, 0.4f, 0.6f, 0.8f, 1.0f };
+        DispatcherTimer timer;
+        Stopwatch stopWatch = new Stopwatch();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,40 +43,65 @@ namespace SimulacionEstufa
 
         private void BtnIniciarSimulacion(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(DiametroTuberiaBox.Text) || string.IsNullOrEmpty(LogintudTuberiaBox.Text) || string.IsNullOrEmpty(PresionDelTanqueBox.Text) || string.IsNullOrEmpty(CantidadMaterialBox.Text) || string.IsNullOrEmpty(ResultadosBox.Text))
-            {
+            if (string.IsNullOrEmpty(DiametroTuberiaBox.Text) || string.IsNullOrEmpty(LogintudTuberiaBox.Text) || string.IsNullOrEmpty(PresionDelTanqueBox.Text) || string.IsNullOrEmpty(CantidadMaterialBox.Text))
                 MessageBox.Show("Por favor, rellena todos los campos.", "Error");
-            }
             else if (posisicionAperturaTanque == 0 || posisicionOrnilla == 0)
-            {
                 MessageBox.Show("La estufa esta apagada.", "Error");
-            }
+            else if (MaterialTxt.Text == "Material")
+                MessageBox.Show("El recipiente esta vacio", "Error");
             else
             {
-                DiametroTuberiaBox.Text = "";
-                LogintudTuberiaBox.Text = "";
-                PresionDelTanqueBox.Text = "";
-                CantidadMaterialBox.Text = "";
-                ResultadosBox.Text = "";
+                estufa.diametroManguera = Convert.ToDouble(DiametroTuberiaBox.Text);
+                estufa.longitudManguera = Convert.ToDouble(LogintudTuberiaBox.Text);
+                estufa.presionTanque = Convert.ToDouble(PresionDelTanqueBox.Text);
+                estufa.eficienciaTanque = eficiencia[posisicionAperturaTanque];
+                estufa.capacidadRecipiente = Convert.ToDouble(CantidadMaterialBox.Text);
+                estufa.eficienciaEstufa = eficiencia[posisicionOrnilla];
+
+                simulacion = new CalculoSimulacion(estufa);
+                EmpezarSimulacion();
             }
-          
         }
 
+        public void EmpezarSimulacion()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Tickevent;
+            stopWatch.Start();
+            timer.Start();
+        }
+
+        public void Tickevent(object sender, EventArgs e)
+        {
+            ResultadosBox.Text = "Resultado:\n";
+            ResultadosBox.Text += $"Tiempo: {stopWatch.Elapsed.ToString(@"hh\:mm\:ss")}\n";
+            ResultadosBox.Text += $"Temperatura: {simulacion.GetTemperatura().ToString("N2")}\n";
+            ResultadosBox.Text += $"Calor necesario: {simulacion.CalorNecesario().ToString("N2")}\n";
+
+            if (simulacion.estufa.temperaturaInicial > simulacion.estufa.temperaturaDeseada)
+                timer.Stop();
+        }
 
         private void BtnAgua(object sender, RoutedEventArgs e)
         {
-            ollaImagen.Source = new BitmapImage(new Uri("/Img/ollaAgua.png", UriKind.Relative));
+            ollaImagen.Source = new BitmapImage(new Uri("/Img/recipientes/ollaAgua.png", UriKind.Relative));
             MaterialTxt.Text = "Agua";
+            estufa.calorEspecifico = 4186.0;
+            estufa.temperaturaDeseada = 100.0;
         }
+
         private void BtnAceite(object sender, RoutedEventArgs e)
         {
-            ollaImagen.Source = new BitmapImage(new Uri("/Img/ollaAceite.png", UriKind.Relative));
+            ollaImagen.Source = new BitmapImage(new Uri("/Img/recipientes/ollaAceite.png", UriKind.Relative));
             MaterialTxt.Text = "Aceite";
+            estufa.calorEspecifico = 2000.0;
+            estufa.temperaturaDeseada = 180.0;
         }
 
         private void btnControlOrnillaUp(object sender, RoutedEventArgs e)
         {
-            if(posisicionOrnilla<5)
+            if (posisicionOrnilla < 5)
                 posisicionOrnilla++;
             ControlOrnillaTXT.Text = posisicionOrnilla.ToString();
             ControlImagen.Source = ImagenControlOrnilla(posisicionOrnilla);
@@ -75,9 +112,10 @@ namespace SimulacionEstufa
                 ornillaImagen.Source = ImagenOrnilla(0);
             }
         }
+
         private void btnControlOrnillaDown(object sender, RoutedEventArgs e)
         {
-            if(posisicionOrnilla>0)
+            if (posisicionOrnilla > 0)
                 posisicionOrnilla--;
             ControlOrnillaTXT.Text = posisicionOrnilla.ToString();
             ControlImagen.Source = ImagenControlOrnilla(posisicionOrnilla);
@@ -91,32 +129,31 @@ namespace SimulacionEstufa
 
         private static BitmapImage ImagenControlOrnilla(int posicion)
         {
-            return new BitmapImage(new Uri("/Img/controlPosicion" + Convert.ToString(posicion) + ".png", UriKind.Relative));
+            return new BitmapImage(new Uri("/Img/perillas/controlPosicion" + Convert.ToString(posicion) + ".png", UriKind.Relative));
         }
-
 
         private static BitmapImage ImagenOrnilla(int posicion)
         {
-            return new BitmapImage(new Uri("/Img/ornilla" + Convert.ToString(posicion) + ".png", UriKind.Relative));
+            return new BitmapImage(new Uri("/Img/ornillas/ornilla" + Convert.ToString(posicion) + ".png", UriKind.Relative));
         }
 
         private void btnAperturaTanqueDerecho(object sender, RoutedEventArgs e)
         {
-
             if (posisicionAperturaTanque < 5)
                 posisicionAperturaTanque++;
+
             ornillaImagen.Source = ImagenOrnilla(posisicionOrnilla);
-           ControlAperturaTanqueTXT.Text= posisicionAperturaTanque.ToString();
+            ControlAperturaTanqueTXT.Text = posisicionAperturaTanque.ToString();
         }
 
         private void btnAperturaTanqueIzquierdo(object sender, RoutedEventArgs e)
         {
             if (posisicionAperturaTanque > 0)
                 posisicionAperturaTanque--;
-            if (posisicionAperturaTanque== 0)
-            {
+
+            if (posisicionAperturaTanque == 0)
                 ornillaImagen.Source = ImagenOrnilla(0);
-            }
+
             ControlAperturaTanqueTXT.Text = posisicionAperturaTanque.ToString();
         }
     }
